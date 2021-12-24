92e42ff323857a97fb8944f7284aeddbc11c7153
Rahul Balamwar <57124734+whorahul@users.noreply.github.com>
2021-12-24 20:00:26 +0530

    Create Reversort.cpp

---
+++ Qualification Round 2021/Reversort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAXN = 100;
+int a[MAXN];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    int cnt = 1;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int j = (int) distance(a, min_element(a + i, a + n));
+            ans += j - i + 1;
+            reverse(a + i, a + j + 1);
+        }
+        printf("Case #%d: %d\n", cnt++, ans);
+    }
+    return 0;
+}
